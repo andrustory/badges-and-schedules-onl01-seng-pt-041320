b4e966b864ff2b8d50a43fe3eb0be51c1c5616b2
Andres Carvajal <jc1993box@hotmail.com>
2020-04-23 03:07:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  badge=[]
+  attendees.each{|name|badge << "Hello, my name is #{name}."}
+  badge
+end
+
+def assign_rooms
+end
