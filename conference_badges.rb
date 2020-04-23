d6c599831487ab19ca426b598a261929381080d5
Andres Carvajal <jc1993box@hotmail.com>
2020-04-23 03:28:07 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,12 +10,9 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(person_name)
-  person_name.each_with_index{|person, room|room.each_with_index}
-  "Hello, #{name}! You'll be assigned to room #{room}!"
+  person_name.each_with_index{|person_name, room_numeber| "Hello, #{person_name}! You'll be assigned to room #{room_number}!"}
 end
 
 
 hash = Hash.new
-%w(cat dog wombat).each_with_index { |item, index|
-  hash[item] = index
-}
+%w(cat dog wombat).each_with_index { |item, index| hash[item] = index}
