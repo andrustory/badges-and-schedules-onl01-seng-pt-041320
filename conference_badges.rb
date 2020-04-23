ff113961a8778dd12180cd0dcc1bd1d9e793acfe
Andres Carvajal <jc1993box@hotmail.com>
2020-04-23 03:34:27 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,7 +11,7 @@ end
 
 def assign_rooms(person_name)
   rooms = []
-  person_name.each_with_index{|person_name, room_number| rooms << "Hello, #{person_name}! You'll be assigned to room #{room_number}!"}
+  person_name.each_with_index{|person_name, room_number| rooms << "Hello, #{person_name}! You'll be assigned to room #{room_number+1}!"}
   rooms
 end
 
