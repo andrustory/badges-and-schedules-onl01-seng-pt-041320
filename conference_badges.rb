5a189e48682501fd494b072d6f4416c1de0a9252
Andres Carvajal <jc1993box@hotmail.com>
2020-04-23 03:28:22 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,6 +13,3 @@ def assign_rooms(person_name)
   person_name.each_with_index{|person_name, room_numeber| "Hello, #{person_name}! You'll be assigned to room #{room_number}!"}
 end
 
-
-hash = Hash.new
-%w(cat dog wombat).each_with_index { |item, index| hash[item] = index}
