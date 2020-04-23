73939b2debd9d00ca8fb51188ded6b8759e55622
Andres Carvajal <jc1993box@hotmail.com>
2020-04-23 03:58:34 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,5 +19,5 @@ def printer (attendees)
   badges = batch_badge_creator (attendees)
   rooms = assign_rooms (attendees)
   badges.each {|badge|puts badges}
-  rooms.each do |room|puts rooms end
+  rooms.each {|room|puts rooms}
 end
