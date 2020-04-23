b0b570eea516bc8b7a27d9fbc610afae2a7a4e14
Andres Carvajal <jc1993box@hotmail.com>
2020-04-23 04:05:02 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,21 +3,21 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(attendees)
+def batch_badge_creator(name)
   badge=[]
-  attendees.each{|name|badge << "Hello, my name is #{name}."}
+  name.each{|name|badge << "Hello, my name is #{name}."}
   badge
 end
 
-def assign_rooms(person_name)
+def assign_rooms(name)
   rooms = []
-  person_name.each_with_index{|person_name, room_number| rooms << "Hello, #{person_name}! You'll be assigned to room #{room_number+1}!"}
+  name.each_with_index{|person_name, room_number| rooms << "Hello, #{person_name}! You'll be assigned to room #{room_number+1}!"}
   rooms
 end
 
-def printer (names)
-  badges = batch_badge_creator (names)
-  rooms = assign_rooms (names)
+def printer (name)
+  badges = batch_badge_creator (name)
+  rooms = assign_rooms (name)
   badges.each {|badge|puts badges}
   rooms.each {|room|puts rooms}
 end
